5189293c468a3375ed12269d849bc638fe48542b
Hubert Sieczka <hsieczka@c3r4s2.42urduliz.com>
2023-09-25 19:44:00 +0200

    Created prototype of memmove

--- 01_libft/ft_memmove.c
+++ 01_libft/ft_memmove.c
@@ -6,8 +6,14 @@
 /*   By: hsieczka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 19:09:35 by hsieczka          #+#    #+#             */
-/*   Updated: 2023/09/12 19:09:36 by hsieczka         ###   ########.fr       */
+/*   Updated: 2023/09/25 19:43:26 by hsieczka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void *ft_memmove(void *dst, const void *src, size_t len); 
 
+void *ft_memmove(void *dst, const void *src, size_t len)
+{
+	
+	return(void *dst)
+}	
