9a1a4b7c3d84da9e02d89afd46d80d7cda8b1e5a
Hubert Sieczka <hsieczka@c3r4s5.42urduliz.com>
2024-02-02 18:32:03 +0100

    added changes to the code

--- 03_get_next_line/get_next_line.c
+++ 03_get_next_line/get_next_line.c
@@ -12,27 +12,41 @@
 
 #include "get_next_line.h"
 
-char	*read_from_file(int fd)
+char	*read_from_file(char *lake_buffer, int fd)
 {
-	int	bytes_read;
-	char	*cup_buffer;
-	static int	count=1;
+	char	*bucket_buffer;
+	int		bytes_read;
 
-	printf("[%d] malloc", count++);
-	cup_buffer = malloc((BUFFER_SIZE+ 1) * sizeof(char));
-	if (cup_buffer == NULL)
+	bucket_buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!bucket_buffer)
 		return (NULL);
-	cup_buffer[3] = '\0';
-	bytes_read = read(fd, cup_buffer, BUFFER_SIZE);
-	if (bytes_read <= 0)
-		return (free(cup_buffer), NULL);
-	return (cup_buffer);
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, bucket_buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (free(bucket_buffer), NULL);
+		bucket_buffer[bytes_read] = '\0';
+		lake_buffer = append_read
+
+	}
+	
 }
 
 char	*get_next_line(int fd)
 {
-	char	*lake_buffer;
+	static char	*lake_buffer;
+	char	*line;
 
-	lake_buffer = read_from_file(fd);
-	return (lake_buffer);
+	if (fd < 0 || read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!lake_buffer);
+		lake_buffer = malloc(1*sizeof(char));
+	if (!ft_strchr(lake_buffer, '\n'))
+		lake_buffer = read_from_file(lake_buffer, fd);
+	if (!lake_buffer)
+		return(free(lake_buffer), NULL);
+	line = extract_line(lake_buffer);
+	lake_buffer = extract_remains(lake_buffer);
+	return (line);
 }
