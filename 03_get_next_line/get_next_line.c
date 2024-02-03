46fb311f08367f4c1c17f411d77701b3293d7a44
hsieczka <huberts94@gmail.com>
2024-02-03 01:00:25 +0100

    Added helper functions and core of gnl

--- 03_get_next_line/get_next_line.c
+++ 03_get_next_line/get_next_line.c
@@ -14,23 +14,28 @@
 
 char	*read_from_file(char *lake_buffer, int fd)
 {
-	char	*bucket_buffer;
+	char	*bucket_buff;
+	char	*tmp;
 	int		bytes_read;
 
-	bucket_buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!bucket_buffer)
-		return (NULL);
+	bucket_buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!bucket_buff)
+		return(NULL);
 	bytes_read = 1;
 	while (bytes_read > 0)
 	{
-		bytes_read = read(fd, bucket_buffer, BUFFER_SIZE);
+		bytes_read = read(fd, bucket_buff, BUFFER_SIZE);
 		if (bytes_read == -1)
-			return (free(bucket_buffer), NULL);
-		bucket_buffer[bytes_read] = '\0';
-		lake_buffer = append_read
-
+			return (free(bucket_buff), NULL);
+		bucket_buff[bytes_read] = '\0';
+		tmp = ft_strjoin(bucket_buff, lake_buffer);
+		free(lake_buffer);
+		lake_buffer = tmp;
+		if (ft_strchr(bucket_buff, '\n'))
+			break;
 	}
-	
+	free(bucket_buff);
+	return (lake_buffer);
 }
 
 char	*get_next_line(int fd)
@@ -40,7 +45,7 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!lake_buffer);
+	if (!lake_buffer)
 		lake_buffer = malloc(1*sizeof(char));
 	if (!ft_strchr(lake_buffer, '\n'))
 		lake_buffer = read_from_file(lake_buffer, fd);
@@ -50,3 +55,13 @@ char	*get_next_line(int fd)
 	lake_buffer = extract_remains(lake_buffer);
 	return (line);
 }
+
+char	*extract_line(char *lake_buffer)
+{
+	char	*tmp;
+	int		size;
+
+	size = 0;
+	
+}
+
--- 03_get_next_line/get_next_line.h
+++ 03_get_next_line/get_next_line.h
@@ -24,8 +24,12 @@
 # endif
 
 char	*get_next_line(int fd);
-char	*read_from_file(int fd);
-
+char	*read_from_file(char *lake_buffer, int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 
 #endif
